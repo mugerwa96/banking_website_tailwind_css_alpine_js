f432ac21d35e5916db346a6182d37b6361c107cc
Mugerwa96 <mugerwaobadiah96@gmail.com>
2023-06-07 20:13:08 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "roboto":['Roboto','sans-serif']
+      },
+      colors:{
+        'primary':"#198a31"
+      }
+    },
+  },
+  plugins: [],
+}
+
